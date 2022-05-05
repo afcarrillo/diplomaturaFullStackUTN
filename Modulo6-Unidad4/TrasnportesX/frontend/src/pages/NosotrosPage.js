445085071cff123396a9f0144f746aa0ae0be508
Alberto Carrillo <albertokarri@gmail.com>
2022-05-05 13:41:26 -0300

    envio de mail formulario contacto

---
+++ Modulo6-Unidad4/TrasnportesX/frontend/src/pages/NosotrosPage.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import '../styles/components/pages/nosotrosPage.css';
+
+const NosotrosPage = (props) => {
+    return (
+        <main className="holder">
+            <div className="historia">
+                <h2>Historia</h2>
+                <p>Wisi forensibus mnesarchum in cum. Per id impetus abhorreant, his no magna definiebas, inani
+                    rationibus in quo. Ut vidisse dolores est, ut quis nominavi mel. Ad pri quod apeirian
+                    concludaturque, id timeam iudicabit rationibus pri. Per nonummy postulant assentior an, mea audiam fabellas deserunt id.</p>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                    laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                    ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+                    dolor in hendrerit in vulputate velit esse molestie consequat</p>
+            </div>
+            <div className="staff">
+                <h2>Staff</h2>
+                <div className="personas">
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros1.jpg" alt="Juan Gomez" />
+                        <h5>Juan Gomez</h5>
+                        <h6>Gerente General</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                            laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros2.jpg" alt="Diana Reyes" />
+                        <h5>Diana Reyes</h5>
+                        <h6>Gerente Comercial</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                            laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros3.jpg" alt="Roberto Zaptos" />
+                        <h5>Roberto Zaptos</h5>
+                        <h6>Gerente de Sistemas</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                            laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros4.jpg" alt="Sandra Mastropiero" />
+                        <h5>Sandra Mastropiero</h5>
+                        <h6>Gerente de Marketing</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                            laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros5.jpg" alt="Luciano Figuero" />
+                        <h5>Luciano Figuero</h5>
+                        <h6>Gerente Logística</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                            laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                    
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default NosotrosPage;
